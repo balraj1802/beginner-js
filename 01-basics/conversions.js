7804185d8efcb10e52753cbadb51ce1e48047adb
balraj1802 <codekarbalraj@gmail.com>
2025-06-17 14:59:22 +0000

    conversions done

---
+++ 01-basics/conversions.js
@@ -0,0 +1,22 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "balraj"
+let str2 = "singh"
+let str3 = str1 + str2
+
+console.log(2+2)
+console.log(2*2)
+console.log(2**3) // 2^3
+
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(2+2+"1")
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter = 100
+gameCounter++ // ++gameCounter  learn about prefix increament and suffix increament
+console.log(gameCounter)
