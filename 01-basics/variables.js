4a3424db331e9b1745c8b87d27da3704e4fab8b1
balraj1802 <codekarbalraj@gmail.com>
2025-06-17 13:45:52 +0000

    variable conversions

---
+++ 01-basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144533
+let accountEmail = "officialbalrajsingh18@gmail.com"
+var acctPasswrd = "1234"
+accountCity = "kanpur"
+
+// accountId = 2 -> cannot be change 
+accountEmail = "codekarbaraj@gmail.com"
+acctPasswrd = "4321"
+accountCity = "Guna"
+/* prefer not to use var variable 
+because issue in block scope and functional scope*/
+console.table([accountEmail, acctPasswrd, accountCity])
