15672cb0244df9284ab9f24c2d39ab2ad02ea96e
balraj1802 <codekarbalraj@gmail.com>
2025-06-18 14:22:35 +0000

    functions01

---
+++ 03-basics/functions.js
@@ -0,0 +1,30 @@
+function addTwoNumbers (number1, number2){
+console.log(number1 + number2);
+}
+const result = addTwoNumbers (3, 5)
+console.log("Result: ", result);
+
+function loginUserMessage (username){
+return `${username} just logged in`
+}
+// console.log(loginUserMessage("hitesh"))
+console.log (loginUserMessage ())
+
+function calculateCartPrice(vall, val2,... num1){
+return num1
+}
+console.log(calculateCartPrice (200,400,500,2000))
+
+const user = {
+username: "hitesh",
+price: 199
+}
+function handleObject (anyobject) {
+console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue (getArIay) {
+return getArray[1]
+}
+console. log (returnSecondValue(myNewArray));
